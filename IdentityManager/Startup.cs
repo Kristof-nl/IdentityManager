a2f7a783d535277e6cade6c22745e5b5bd6db944
Krzysztof Hermaszewski <hermaszewski.krzysztof@gmail.com>
2022-01-22 21:33:20 +0100

    Add Register and Login controllers

--- IdentityManager/Startup.cs
+++ IdentityManager/Startup.cs
@@ -2,6 +2,7 @@ using IdentityManager.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +28,8 @@ namespace IdentityManager
         {
             services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+
             services.AddControllersWithViews();
 
         }
@@ -48,6 +51,8 @@ namespace IdentityManager
             app.UseStaticFiles();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
